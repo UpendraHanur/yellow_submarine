2510baee8870ce6c17d17168f3791ed48df48965
UpendraHanur <86529603+UpendraHanur@users.noreply.github.com>
2024-02-11 07:31:52 -0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,17 @@ class HelloWorld
   public static void main(String args[])
     {
       Sysem.out.println("Welcome to the world of DevOps");
+
+      int a = 10;
+      int b = 2;
+      int c = a+b;
+      System.out.println("Addition of a and b is "+c);
+      int d = a-b;
+      System.out.println("Subtraction of a and b is "+d);
+      int e = a*b;
+      System.out.println("Multiplication of a and b is "+e);
+      int f = a/b;
+      System.out.println("Division of a and b is "+f);
     }
 }
 
